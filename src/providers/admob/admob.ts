8965049e4323047aa9020f83afb17e42eff1d037
ezekiel_tradably <ezekiel.tradably@gmail.com>
2018-12-18 14:22:46 +0800

    new project

--- src/providers/admob/admob.ts
+++ src/providers/admob/admob.ts
@@ -12,51 +12,66 @@ export class AdmobFreeProvider {
 
   private clickToShowAds = 5; //<-- number of time user click on the app before the ads shown
 
-  public bannerConfig: AdMobFreeBannerConfig = {
-      id : "ca-app-pub-2249157507977141/9983566656",
-      autoShow: true,
-      bannerAtTop : false,
-      isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
-   };
+  public bannerConfig: AdMobFreeBannerConfig;
 
-   public InterstitialConfig : AdMobFreeInterstitialConfig = {
-     id : "ca-app-pub-2249157507977141/3362317982",
-     autoShow:true,
-     isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
-   }
+   public InterstitialConfig : AdMobFreeInterstitialConfig;
 
-   public RewardVideoConfig : AdMobFreeRewardVideoConfig = {
-     id : "ca-app-pub-2249157507977141/9663997250",
-     autoShow : true,
-     isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
-   }
+   public RewardVideoConfig : AdMobFreeRewardVideoConfig;
 
    private isBannerShown = false;
 
   constructor(private admob:AdMobFree,public platform: Platform) {
     this.platform.ready().then(()=>{
+
+        // Setup admob for android 
         if(this.platform.is('android')) {
-          this.bannerId = 'ca-app-pub-2249157507977141/9983566656';//<-- Past your banner admob id  for android here
-          this.InterstitialId = 'ca-app-pub-2249157507977141/3362317982';//<-- Past your Interstitial admob id for android here
-          this.VideoID = "ca-app-pub-2249157507977141/9663997250";//<-- Past your video admob id for android here
-          // this.bannerConfig.id = this.bannerId;
-          // this.InterstitialConfig.id = this.InterstitialId;
-          // this.VideoID.id = this.VideoID;
+          this.bannerConfig = {
+            id : "",
+            autoShow: true,
+            bannerAtTop : false,
+            isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
+          }
+          this.InterstitialConfig = {
+              id : "",
+              autoShow:true,
+              isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
+          }
+
+          this.RewardVideoConfig = {
+              id : "",
+              autoShow : true,
+              isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
+          }
           console.log("config admob success");
         }
     
+        // / Setup admob for android  IOS
         if(this.platform.is('ios')) {
-          this.bannerId = "ca-app-pub-2249157507977141/7720360192";//<-- Past your banner admob id  for ios here
-          this.InterstitialId = "ca-app-pub-2249157507977141/6849706620";//<-- Past your Interstitial admob id for ios here
-          this.VideoID = "ca-app-pub-2249157507977141/1510175066";//<-- Past your video admob id for ios here
-          // this.bannerConfig.id = this.bannerId;
-          // this.InterstitialConfig.id = this.InterstitialId;
-          // this.VideoID.id = this.VideoID;
+          this.bannerConfig = {
+            id : "",
+            autoShow: true,
+            bannerAtTop : false,
+            isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
+          }
+          this.InterstitialConfig = {
+              id : "",
+              autoShow:true,
+              isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
+          }
+
+          this.RewardVideoConfig = {
+              id : "",
+              autoShow : true,
+              isTesting:false,//<- if you want just to test if admob is working , you can set the value to true
+          }
           console.log("config admob success");
         }
+
+
         this.admob.banner.config(this.bannerConfig);
         this.admob.interstitial.config(this.InterstitialConfig);
         this.admob.rewardVideo.config(this.RewardVideoConfig);
+        
     })
   }
 
