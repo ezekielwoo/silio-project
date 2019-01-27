7f6c7819fabf44965ada84e7ed57c7f8de7c44c2
ezekiel_tradably <ezekiel.tradably@gmail.com>
2019-01-27 21:06:12 +0800

    final final

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { RegisterPage } from '../register/register';
-import { LoginPage } from '../login/login';
-<<<<<<< HEAD
-import { TabsPage } from '../tabs/tabs';
-import { Storage } from '@ionic/storage';
-=======
- 
->>>>>>> e10d4f4d8e8414132fd46206ee350b33bca278c0
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {RegisterPage} from '../register/register';
+import {LoginPage} from '../login/login';
+import {TabsPage} from '../tabs/tabs';
+import {Storage} from '@ionic/storage';
+
 /**
  * Generated class for the MainPage page.
  *
@@ -22,35 +19,27 @@ import { Storage } from '@ionic/storage';
 })
 export class MainPage {
 
-<<<<<<< HEAD
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
-=======
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
->>>>>>> e10d4f4d8e8414132fd46206ee350b33bca278c0
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MainPage');
-<<<<<<< HEAD
-
     this.storage.get("email").then((val) => {
       console.log(val);
-       if(val != null){
-         this.navCtrl.setRoot(TabsPage);
-       }
-       else if(val == "" || val == null || val == undefined){
-          console.log(val);
-       }
+      if (val != null) {
+        this.navCtrl.setRoot(TabsPage);
+      }
+      else if (val == "" || val == null || val == undefined) {
+        console.log(val);
+      }
     })
-=======
->>>>>>> e10d4f4d8e8414132fd46206ee350b33bca278c0
   }
 
-  goRegister(){
+  goRegister() {
     this.navCtrl.push(RegisterPage);
   }
 
-  goLogin(){
+  goLogin() {
     this.navCtrl.push(LoginPage);
   }
 
